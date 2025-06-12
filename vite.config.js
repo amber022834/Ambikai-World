61e3de8d65a8a001afc6f2e51696cc97ef2b8ae9
amber022834 <sodagreen0228@gmail.com>
2025-06-13 01:09:30 +0800

    config更新

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/ambikai-world-demo-world/',
+  base: '/Ambikai-world/',
   plugins: [react()],
 
 })
